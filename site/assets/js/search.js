b0a241e4e187644f275c2df03a0acbd8116b76be
Максим Яшунин <m.yashunin@askona.ru>
2023-05-02 14:09:11 +0300

    переход на относительные ссылки чтобы работало при деплое на любой домен

--- site/assets/js/search.js
+++ site/assets/js/search.js
@@ -25,12 +25,7 @@
       return items.map(item => {
         const liveUrl = 'https://getbootstrap.com/'
 
-        item.url = window.location.origin.startsWith(liveUrl) ?
-          // On production, return the result as is
-          item.url :
-          // On development or Netlify, replace `item.url` with a trailing slash,
-          // so that the result link is relative to the server root
-          item.url.replace(liveUrl, '/')
+        item.url = item.url.replace(liveUrl, '/')
 
         // Prevent jumping to first header
         if (item.anchor === 'content') {
